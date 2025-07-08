4a3adf587a47bd92dd4d78c35737bb8bc5b1e30d
Mosam04 <bsc-com-ne-33-23@unima.ac.mw>
2025-07-08 14:48:27 +0200

    setting changed

--- Django-School-Management-System/school_app/settings.py
+++ Django-School-Management-System/school_app/settings.py
@@ -25,9 +25,8 @@ SECRET_KEY = "__$1ud47e&nyso5h5o3fwnqu4+hfqcply9h$k*h2s34)hn5@nc"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
-
+ALLOWED_HOSTS = ['.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
